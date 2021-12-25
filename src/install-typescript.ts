e0f913dd889cf737759fafc33276fe5d8831b4ca
pnskh112 <pnskh112pnskh112pnskh112@gmail.com>
2021-12-25 23:12:52 +0900

    Add ts-node-dev

--- src/install-typescript.ts
+++ src/install-typescript.ts
@@ -1,5 +1,7 @@
-let message: string = 'Hello Typescript!';
-# node の 型情報ファイルが必要
-# npm info @types/node
-# npm install @types/node@x.x.x
+let message: string = `Let's Study Typescript!`;
+/*
+* node の 型情報ファイルが必要
+* npm info @types/node
+* npm install @types/node@x.x.x
+*/
 console.log({ message });
