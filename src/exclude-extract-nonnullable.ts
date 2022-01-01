565da2c849118cf4b8351a4d5d73f92e98f31207
pnskh112 <pnskh112pnskh112pnskh112@gmail.com>
2022-01-02 00:57:50 +0900

    Revival exclude-extract-nonnullable

---
+++ src/exclude-extract-nonnullable.ts
@@ -0,0 +1,16 @@
+export {};
+
+type DebugType = () => void;
+type SomeTypes = string | number | DebugType;
+type FunctionType = Exclude<SomeTypes, string | number>;
+type NonFunctionType = Exclude<SomeTypes, DebugType>;
+// eslint-disable-next-line @typescript-eslint/ban-types
+type TypeExcludeFunction = Exclude<SomeTypes, Function>;
+
+type FunctionTypeByExtract = Extract<SomeTypes, DebugType>;
+type NonFunctionTypeByExtract = Extract<SomeTypes, string | number>;
+// eslint-disable-next-line @typescript-eslint/ban-types
+type FunctionTypeExtractingFunction = Extract<SomeTypes, Function>;
+
+type NullableTypes = string | number | null | undefined;
+type NonNullableTypes = NonNullable<NullableTypes>;
